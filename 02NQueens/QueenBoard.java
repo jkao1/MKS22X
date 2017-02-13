1e7f26277dfefae102e9996c9796dda0cd8b0d94
jkao1 <jkao1@stuy.edu>
2017-02-13 00:02:51 -0500

    progresse

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,12 +1,19 @@
 public class QueenBoard {
 
+    public static boolean DEBUG_MODE_ON;
+
     public int[][] board;
     private int solutionCount;
 
     public QueenBoard(int size) {
-	board = new int[size][size];
+        this( size,false );
     }
-
+    public QueenBoard(int size, boolean dbo) {
+	board = new int[size][size];
+	DEBUG_MODE_ON = dbo;
+	solutionCount = 0;
+    }
+    
     /**
      * precondition: board is filled with 0's only.
      * @return false when the board is not solveable. true otherwise.
@@ -22,28 +29,99 @@ public class QueenBoard {
 
     private boolean solveH(int col)
     {
+	if ( col >= board.length ) {
+	    solutionCount++;
+	    return true;
+	}
 	for (int row = 0; row < board.length; row++)
 	{
+	    System.out.println("row "+row+", col "+col);
 	    if (queenPlaceable( row,col )) {
+		System.out.println("queen is placeable.");
+		addQueen( row,col );
+		System.out.println(this);
+		if (solveH( ++col )) {
+		    return true;
+		} else {
+		    removeQueen( row,col );
+		    System.out.println(this+"\nqueen removed.");
+		}
 	    }
+	    System.out.println();
 	}
+	return false;
     }
 
     private boolean queenPlaceable(int row, int col)
     {
-	for (int r = 0; r < board.length; r++)
-	    if ( board[r][col] != 0 )
-		return false;
-	for (int c = 0; c < board.length; c++)
-	    if ( board[row][c] != 0 )
-		return false;
+	return board[ row ][ col ] == 0;
+    }
 
-	int topLeft = boards.length * (row - col);
+    private void addQueen(int row, int col)
+    {
+	for (int i = 0; i < board.length * board.length; i++)
+	{
+	    if ( i % board.length == col ||
+		 i / board.length == row ||
+		 Math.abs( i / board.length - row ) == Math.abs( i % board.length - col ) )
+		{
+		if ( board[ i / board.length ][ i % board.length ] != -1 )
+		    board[ i / board.length ][ i % board.length ]++;
+	    }
+	}
+	board[ row ][ col ] = -1;
+    }
+
+    private void removeQueen(int row, int col)
+    {
+	board[ row ][ col ] = 0;	
+	for (int i = 0; i < board.length * board.length; i++)
+	    {
+		if ( ( i % board.length == col || i / board.length == row || Math.abs( i / board.length - row ) == Math.abs( i % board.length - col )) && i != board.length * row + col )
+		    {
+			if ( board[ i / board.length ][ i % board.length ] != -1 ) 
+			    board[ i / board.length ][ i % board.length ]--;
+			else
+			    board[ row ][ col ]++;
+		    }
+	    }
+    }
+	    
+
+    public String toString()
+    {
+	String o = "";
+	for (int[] row : board) {
+	    for (int val : row) {
+		if ( val == -1 )
+		    o += "Q";
+		else
+		    o += "_";
+	    }
+	    o += "\n";
+	}
+
+	if (DEBUG_MODE_ON) {
+	    o += "\n";
+	    for (int[] row : board) {
+		for (int val : row) {
+		    String v = String.valueOf( val );
+		    if ( v.length() > 1 )
+			o += v + " ";
+		    else
+			o += v + "  ";
+		}
+		o += "\n";
+	    }
+	}
+	return o;
+    }
 
     public static void main(String[] args) {
-	QueenBoard q = new QueenBoard(4);
-	int current = (q.board.length * 3 + 3);
-	System.out.println( current);
+	QueenBoard q = new QueenBoard( 4,true );
+	q.addQueen(0,0);
+	q.removeQueen(0,0);
+	System.out.println(q.solve());
     }
     
 	
