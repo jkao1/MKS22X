521d15b57d84c82ca7260701154f2115e03f8f1a
jkao1 <jasonkao85@gmail.com>
2017-02-16 14:49:53 -0500

    progress 02-16-17

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -3,7 +3,7 @@ public class KnightBoard {
     private int[][] POSSIBLE = {
 	{ 1,2 }, { 1,-2 }, {-1,2}, {-1,-2},
 	{ 2,1 }, { 1,-2 }, {-1,2}, {-1,-2},
-    }
+    };
     private int[][] board;
 
     public KnightBoard(int startingRows, int startingCols)
@@ -13,9 +13,13 @@ public class KnightBoard {
 
     public String toString() {
 	String o = "";
-	for (int[] row : board) {
-	    for (int num : row) {
-		o += num + "  ";
+	for (int r = 0; r < board.length; r++) {
+	    for (int c = 0; c < board[0].length; c++) {
+		if (board[r][c] < 10)
+		    o += " " + board[r][c];
+		else
+		    o += board[r][c];
+		o += " ";
 	    }
 	    o += "\n";
 	}
@@ -28,22 +32,25 @@ public class KnightBoard {
 
     private boolean solveH(int row, int col, int level)
     {
-	if (level > board.length * board.length)
+	if (row < 0 || row >= board.length || col < 0 || col >= board.length) {
+	    System.out.println("out of bounds");
+	    return false;
+	}
+	if (level >=board.length * board.length)
 	    return true;
+	
 	if ( knightPlaceable( row,col )) {
 	    addKnight( row,col,level );
-	    for (int r : ONES) {
-		for (int c : TWOS) {
-		    if (row + r > 0 && row + r < board.length &&
-			col + c > 0 && col + c < board.length) {
-			if (solveH( row+r, col+c, ++level ))
-			    return true;
-			else
-			    removeKnight( row,col );
-		    }
-		}
-		System.out.println(this);
+	    System.out.println(this);
+	    for (int[] move : POSSIBLE) {
+	        int r = move[0];
+		int c = move[1];
+		System.out.println("moving to " + (row+r ) + ","+(col+c));
+		if (solveH( row+r, col+c, level+1 ))
+		    return true;
+		System.out.println("move doesnt work");
 	    }
+	    removeKnight (row,col);System.out.println("removed "+(row  )+","+(col));
 	}
 	return false;
     }
