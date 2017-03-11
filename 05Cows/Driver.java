2246556b5c9f03143c3101ac40a1061fe29f52cf
jkao1 <jkao1@stuy.edu>
2017-03-10 19:42:21 -0500

    3

---
+++ 05Cows/Driver.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        USACO u = new USACO();
+        System.out.println( u.bronze( "makelake.in" ) );
+    }
+}
--- 05Cows/USACO.java
+++ 05Cows/USACO.java
@@ -1,45 +1,52 @@
-import java.util.io.*;
+import java.io.*;
 import java.util.*;
 
 public class USACO {
 
     private int[][] lake;
-    private ArrayList<int[]> instructions;
 
     public int bronze(String filename) {
 
         Scanner in = null;
         try {
             File f = new File( filename );
-            Scanner in = new Scanner( f );
+            in = new Scanner( f );
         } catch (Exception e) {
             System.out.println("File not found.");
             System.exit(0);
         }
 
-        Scanner header = in.nextLine();
+        Scanner header = new Scanner( in.nextLine() );
         int rows = header.nextInt();
         int cols = header.nextInt();
-        int elevation = header.nextInt();
+        int finalElevation = header.nextInt();
 
         lake = new int[ rows ][ cols ];
         for (int r = 0; r < rows; r++) {
-            Scanner row = in.nextLine();
+            Scanner row = new Scanner( in.nextLine() );
             for (int c = 0; c < cols; c++) {
                 lake[r][c] = row.nextInt();
             }
         }
 
         while (in.hasNextLine()) {
-            Scanner instruction = in.nextLine();
-            int r = instruction.nextInt();
-            int c = instruction.nextInt();
+            Scanner instruction = new Scanner( in.nextLine() );
+            int row = instruction.nextInt() - 1;
+            int col = instruction.nextInt() - 1;
             int stomp = instruction.nextInt();
-            int stompHeight = findAndStompMax( r,c,stomp );
+            int stompHeight = findAndStompMax( row, col, stomp );
+            for (int r = row; r < row + 3; r++) {
+                for (int c = col; c < col + 3; c++) {
+                    if ( lake[r][c] > stompHeight )
+                        lake[r][c] = stompHeight;
+                }
+            }
         }
+
+        return getVolume( finalElevation );
     }
 
-    public int findAndStompMax(int row, int col, int stomp){
+    private int findAndStompMax(int row, int col, int stomp) {
         int max = lake[row][col], max_r = row, max_c = col;
         for (int r = row; r < row + 3; r++) {
             for (int c = col; c < col + 3; c++) {
@@ -54,17 +61,29 @@ public class USACO {
         return max - stomp;
     }
 
-    public int findVolume(int el)
-    {
-        int o = 0;
-        for (int r = 0; r < lake.length; r++) {
-            for (int c = 0; c < lake[r].length; c++) {
-                int hi = max - lake[r][c];
-                if (hi > 0) {
-                    o += hi;
+    private int getVolume(int finalElevation) {
+        int totalDepth = 0;
+        for (int[] rows : lake) {
+            for (int x : rows) {
+                if (x < finalElevation) {
+                    totalDepth += finalElevation - x;
                 }
             }
         }
-        return o * 66 * 66;
+        return totalDepth * 72 * 72;
+    }
+
+    public String lakeToString() {
+        String o = "";
+        for (int[] row : lake) {
+            for (int c : row) {
+                o += c;
+                for (int i = 0; i < 4 - String.valueOf( c ).length(); i++) {
+                    o += " ";
+                }
+            }
+            o += "\n";
+        }
+        return o;
     }
 }
