d42651d96603c3a5ea45ea110df9ce6edd732eed
jkao1 <jasonkao85@gmail.com>
2017-03-21 14:48:56 -0400

    gdi

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,11 +1,11 @@
 public class Quick {
 
-    public static void quickselect(int[] ary, int k)
+    public static int quickselect(int[] ary, int k)
     {
-        quickselectH(ary, k, 0, ary.length );
+        return quickselectH(ary, k, 0, ary.length-1,0 );
     }
 
-    public static int quickselectH(int[] ary, int k, int left, int right)
+    public static int quickselectH(int[] ary, int k, int left, int right, int t)
     {
         int randomIndex = left + (int) ( Math.random() * ( right - left + 1 ));
         swap(randomIndex, left, ary);
@@ -25,6 +25,19 @@ public class Quick {
                 i++;
             }
         }
+
+	if (t > 20) return 0;
+	System.out.println(v);
+	if (k < lt) {
+	    System.out.println("taking the left side." + left + "," +(lt-1));
+	    print(ary);
+	    return quickselectH(ary, k, left, lt - 1,t+1);
+	} else if (k > gt) {
+	    System.out.println("taking the right side." + gt+1 + "," +right);
+	    print(ary);
+	    return quickselectH(ary, k, gt + 1, right, t+1);
+	}
+	return ary[k];
     }
 
     private static void swap(int x, int y, int[] ary) {
@@ -41,30 +54,8 @@ public class Quick {
         System.out.println(o);
     }
 
-    private static void coolPrint(int[] ary, int wall) {
-        String o = "";
-        for (int i = 0; i < ary.length; i++) {
-            if (i == wall)
-                o += "| " + ary[i] + " ";
-            else
-                o += ary[i] + " ";
-        }
-        System.out.println(o);
-    }
-
     public static void main(String[] args) {
-        int[] ary = { 5, 15, 12, 18 ,5, 0, 13, 3, 20 ,19 };
-        part(ary,0,ary.length-1);
-        /*
-        System.out.println("random array testing-----");
-        for (int i = 0; i < 2; i++) {
-            System.out.println("array #" + i);
-            int[] ary = new int[10];
-            for (int j = 0; j < 10; j++) {
-                int randomNum = (int) ( Math.random() * 20 );
-                ary[j] = randomNum;
-            }
-            part( ary, 0, ary.length-1);
-            }*/
+        int[] ary = { 50, 15, 12, 18 ,5, 0, 13, 3, 20 ,19 };
+        System.out.println(quickselect(ary,0));
     }
 }
