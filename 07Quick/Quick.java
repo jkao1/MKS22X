6322b27e8845aeb3d7f972a7c0f3ac4bb2b1f163
jkao1 <jkao1@stuy.edu>
2017-03-21 17:53:23 -0400

    AFTER 2 HRS

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -2,18 +2,24 @@ public class Quick {
 
     public static int quickselect(int[] ary, int k)
     {
-        return quickselectH(ary, k, 0, ary.length-1,0 );
+        int start = 0, end = ary.length - 1, pivot = 0;
+        while (k != pivot) {
+            pivot = part(ary, start, end);
+            if (k < pivot) {
+                end = pivot - 1;
+            } else if (k > pivot) {
+                start = pivot + 1;
+            }
+        }
+        return ary[k];
     }
 
-    public static int quickselectH(int[] ary, int k, int left, int right, int t)
-    {return 0;}
-    public static void part(int[] ary, int start, int end)
+    public static int part(int[] ary, int start, int end)
     {
         int randomIndex = start + (int) ( Math.random() * ( end - start + 1 ));
         swap(randomIndex, start, ary);
         int v = ary[start];
-        System.out.println(v);
-        int i = start + 1, lt = start, gt = end - 1;
+        int i = start, lt = start, gt = end;
         while (i <= gt) {
             if (ary[i] < v) {
                 swap(i, lt, ary);
@@ -26,7 +32,7 @@ public class Quick {
                 i++;
             }
         }
-        print(ary);
+        return lt;
     }
 
     private static void swap(int x, int y, int[] ary) {
@@ -43,8 +49,23 @@ public class Quick {
         System.out.println(o);
     }
 
+    private static void print(int[] ary, int s, int e) {
+        String o = "";
+        for (int i = 0; i < ary.length; i++) {
+            if (i == s) {
+                o += "| " + ary[i] + " ";
+            } else if (i == e) {
+                o += ary[i] + " | ";
+            } else {
+                o += ary[i] + " ";
+            }
+        }
+        System.out.println(o);
+    }
+
     public static void main(String[] args) {
         int[] ary = { 0,1,2,3,4,5,6,7,8,9};
-        part(ary,0,ary.length);
+        int[] d = {999,999,999,5,7,3,8,4,9,1,2,0,999,999,999};
+        System.out.println(quickselect(d, Integer.parseInt(args[0])));
     }
 }
