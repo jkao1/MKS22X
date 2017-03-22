ee872747e18e694478564462a4ef2663704de4b1
jkao1 <jkao1@stuy.edu>
2017-03-22 06:56:54 -0400

    tested and tested

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,4 +1,38 @@
+import java.util.*;
+
 public class Quick {
+
+    public static void quicksort(int[] ary)
+    {
+        quicksortH(ary, 0, ary.length - 1);
+    }
+
+    public static void quicksortH(int[] ary, int start, int end)
+    {
+        int randomIndex = start + (int) ( Math.random() * ( end - start + 1 ));
+        swap(randomIndex, start, ary);
+        int v = ary[start];
+        int i = start, lt = start, gt = end;
+        while (i <= gt) {
+            if (ary[i] < v) {
+                swap(i, lt, ary);
+                i++;
+                lt++;
+            } else if (ary[i] > v) {
+                swap(i, gt, ary);
+                gt--;
+            } else {
+                i++;
+            }
+        }
+
+        // here marks the end of partition and the beginning of recursion
+
+        if (end > start) {
+            quicksortH(ary, gt, end);
+            quicksortH(ary, start, lt - 1);
+        }
+    }
 
     public static int quickselect(int[] ary, int k)
     {
@@ -40,4 +74,5 @@ public class Quick {
         ary[x] = ary[y];
         ary[y] = temp;
     }
+
 }
