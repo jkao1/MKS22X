5244a23d468422c78bcf647cdb634df211af9173
Jason Kao <jasonkao@Jasons-MacBook-Pro.local>
2017-04-19 12:44:25 -0400

    pushing

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,15 @@
+public class MyDeque {
+
+    private int[] ary;
+    private int size;
+    private int front, back;
+
+    public MyDeque()
+    {
+        ary = new int[10];
+        size = 0;
+        front = 0;
+        back = 0;
+    }
+
+}
