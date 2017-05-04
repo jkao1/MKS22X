6e98813aeffcc80e0f2f9e998224d742687adcaf
jkao1 <jasonkao85@gmail.com>
2017-05-04 14:48:04 -0400

    progress

---
+++ 13MyHeap/Driver.java
@@ -0,0 +1,22 @@
+public class Driver {
+
+    public static void main(String[] args)
+    {
+	MyHeap h = new MyHeap(true);
+	h.add("1");
+	p(h);
+	h.add("2");
+	p(h);
+	h.add("3");
+	p(h);
+	h.remove();
+	p(h);
+	h.remove();
+	p(h);
+    }
+
+    public static void p(MyHeap h) {
+	System.out.println(h);
+    }
+
+}
--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,14 +1,16 @@
+import java.util.*;
+
 public class MyHeap {
 
     private static int HEAP_ORDER; // 1 if MaxHeap, -1 if MinHeap
 
-    private ArrayList<String> ary;
+    private String[] ary;
     private int size;
 
     public MyHeap()
     {
-	ary = new ArrayList<>();
-	ary.add(""); // placeholder for math stuff;
+	ary = new String[10];
+	size = 0;
     }
 
     public MyHeap(boolean HEAP_ORDER)
@@ -22,10 +24,60 @@ public class MyHeap {
     }
 
     public void add(String s)
-    {
-	ary.add(s);
+    {	
 	size++;
+	ary[size] = s;
+	pushUp();
     }
 
-    public 
+    private void pushUp()
+    {
+	int i = size;
+	if (size == 1) {
+	    return;
+	}
+	while (ary[i].compareTo(ary[ i / 2]) * HEAP_ORDER > 0) {
+	    swap(i, i / 2);
+	}
+    }
+
+    public String remove()
+    {
+	String output = ary[1];
+	ary[1] = ary[size];
+	size--;
+	pushDown();
+	return output;
+    }
+
+    private void pushDown()
+    {
+	int i = 1;
+	// if should be pushed down
+	boolean left = ary[i].compareTo(ary[i * 2]) * HEAP_ORDER < 0;
+	boolean right = ary[i].compareTo(ary[i * 2 + 1]) * HEAP_ORDER < 0;
+	while (left || right) {
+	    if (left) {
+		swap(i, i * 2);
+	    } else if (right) {
+		swap(i, i * 2 + 1);
+	    }
+	    left = ary[i].compareTo(ary[i * 2]) * HEAP_ORDER < 0;
+	    right =  ary[i].compareTo(ary[i * 2 + 1]) * HEAP_ORDER < 0;
+	}
+    }
+
+    private void swap(int a, int b) 
+    {
+	String temp = ary[a];
+	ary[a] = ary[b];
+	ary[b] = temp;
+    }
+
+    public String toString()
+    {
+	return Arrays.toString(ary);
+    }
+
+}
 	
