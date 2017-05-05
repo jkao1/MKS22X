b5985c5049aa9e24a773ba364e8c33bccc49f4c3
jkao1 <jkao1@stuy.edu>
2017-05-04 23:34:47 -0400

    hea

--- 13MyHeap/Driver.java
+++ 13MyHeap/Driver.java
@@ -2,21 +2,21 @@ public class Driver {
 
     public static void main(String[] args)
     {
-	MyHeap h = new MyHeap(true);
-	h.add("1");
-	p(h);
-	h.add("2");
-	p(h);
-	h.add("3");
-	p(h);
-	h.remove();
-	p(h);
-	h.remove();
-	p(h);
+        MyHeap h = new MyHeap(true);
+        h.add("1");
+        p(h);
+        h.add("2");
+        p(h);
+        h.add("3");
+        p(h);
+        h.add("9");
+        p(h);
+        h.add("3");
+        p(h);
     }
 
     public static void p(MyHeap h) {
-	System.out.println(h);
+        System.out.println(h);
     }
 
 }
--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -9,75 +9,74 @@ public class MyHeap {
 
     public MyHeap()
     {
-	ary = new String[10];
-	size = 0;
+        ary = new String[10];
+        size = 0;
     }
 
     public MyHeap(boolean HEAP_ORDER)
     {
-	this();
-	if (HEAP_ORDER) {
-	    this.HEAP_ORDER = 1;
-	} else {
-	    this.HEAP_ORDER = -1;
-	}
+        this();
+        if (HEAP_ORDER) {
+            this.HEAP_ORDER = 1;
+        } else {
+            this.HEAP_ORDER = -1;
+        }
     }
 
     public void add(String s)
-    {	
-	size++;
-	ary[size] = s;
-	pushUp();
+    {
+        size++;
+        ary[size] = s;
+        pushUp();
     }
 
     private void pushUp()
     {
-	int i = size;
-	if (size == 1) {
-	    return;
-	}
-	while (ary[i].compareTo(ary[ i / 2]) * HEAP_ORDER > 0) {
-	    swap(i, i / 2);
-	}
+        int i = size;
+        if (size == 1) {
+            return;
+        }
+        while (ary[i].compareTo(ary[ i / 2]) * HEAP_ORDER > 0) {
+            swap(i, i / 2);
+        }
     }
 
     public String remove()
     {
-	String output = ary[1];
-	ary[1] = ary[size];
-	size--;
-	pushDown();
-	return output;
+        String output = ary[1];
+        ary[1] = ary[size];
+        pushDown();
+        size--;
+        return output;
     }
 
     private void pushDown()
     {
-	int i = 1;
-	// if should be pushed down
-	boolean left = ary[i].compareTo(ary[i * 2]) * HEAP_ORDER < 0;
-	boolean right = ary[i].compareTo(ary[i * 2 + 1]) * HEAP_ORDER < 0;
-	while (left || right) {
-	    if (left) {
-		swap(i, i * 2);
-	    } else if (right) {
-		swap(i, i * 2 + 1);
-	    }
-	    left = ary[i].compareTo(ary[i * 2]) * HEAP_ORDER < 0;
-	    right =  ary[i].compareTo(ary[i * 2 + 1]) * HEAP_ORDER < 0;
-	}
+        int i = 1;
+        // if should be pushed down
+        boolean left = i * 2 < size && ary[i * 2] != null && ary[i].compareTo(ary[i * 2]) * HEAP_ORDER < 0;
+        boolean right = i * 2 + 1 < size && ary[i * 2 + 1] != null && ary[i].compareTo(ary[i * 2 + 1]) * HEAP_ORDER < 0;
+        while (left || right) {
+            if (left) {
+                swap(i, i * 2);
+            } else if (right) {
+                swap(i, i * 2 + 1);
+            }
+            left = i * 2 < size && ary[i * 2] != null && ary[i].compareTo(ary[i * 2]) * HEAP_ORDER < 0;
+            right = i * 2 + 1 < size && ary[i * 2 + 1] != null && ary[i].compareTo(ary[i * 2 + 1]) * HEAP_ORDER < 0;
+        }
     }
 
-    private void swap(int a, int b) 
+    private void swap(int a, int b)
     {
-	String temp = ary[a];
-	ary[a] = ary[b];
-	ary[b] = temp;
+        String temp = ary[a];
+        ary[a] = ary[b];
+        ary[b] = temp;
     }
 
     public String toString()
     {
-	return Arrays.toString(ary);
+        return Arrays.toString(ary);
     }
 
 }
-	
