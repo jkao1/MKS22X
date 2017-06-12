bda20d0b6cc978049c42c53f852241f9b7d6b44c
jkao1 <jkao1@stuy.edu>
2017-06-12 16:05:14 -0400

    final

--- 12ExpressionTree/ExpressionTree.java
+++ 12ExpressionTree/ExpressionTree.java
@@ -1,58 +1,58 @@
 public class ExpressionTree {
-  
+
     /*return the value of the specified expression tree*/
     public double evaluate()
     {
-	if (isValue()) {
-	    return getValue();
-	} else if (isOp()) {
-	    return perform(getOp(), getLeft().evaluate(), getRight().evaluate());
-	} else {
-	    return -1.0;
-	}
+        if (isValue()) {
+            return getValue();
+        } else if (isOp()) {
+            return perform(getOp(), getLeft().evaluate(), getRight().evaluate());
+        } else {
+            return -1.0;
+        }
     }
 
     private double perform(char op, double a, double b)
     {
-	if (op == '+') {
-	    return a + b;
-	} else if (op == '-') {
-	    return a - b;
-	} else if (op == '*') {
-	    return a * b;
-	} else if (op == '/') {
-	    return a / b;
-	} else if (op == '%') {
-	    return a % b;
-	} else {
-	    return 0.0;
-	}
+        if (op == '+') {
+            return a + b;
+        } else if (op == '-') {
+            return a - b;
+        } else if (op == '*') {
+            return a * b;
+        } else if (op == '/') {
+            return a / b;
+        } else if (op == '%') {
+            return a % b;
+        } else {
+            return 0.0;
+        }
     }
-  
+
     /*return the expression as an infix notation string with parenthesis*/
     /* The sample tree would be: "( 3 + (2 * 10))"     */
     public String toString()
     {
-	if (isValue()) {
-	    return getValue() + "";
-	} else if (isOp()) {
-	    return "(" + getLeft() + " " + getOp() + " " + getRight() + ")";
-	} else {
-	    return "toString() ERROR";
-	}
+        if (isValue()) {
+            return getValue() + "";
+        } else if (isOp()) {
+            return "(" + getLeft() + " " + getOp() + " " + getRight() + ")";
+        } else {
+            return "toString() ERROR";
+        }
     }
-  
+
     /*return the expression as a postfix notation string without parenthesis*/
     /* The sample tree would be: "3 2 10 * +"     */
     public String toStringPostfix()
     {
         if (isValue()) {
-	    return getValue() + "";
-	} else if (isOp()) {
-	    return getLeft().toStringPostfix() + " " + getRight().toStringPostfix() + " " + getOp();
-	} else {
-	    return "toStringPostfix() ERROR";
-	}
+            return getValue() + "";
+        } else if (isOp()) {
+            return getLeft().toStringPostfix() + " " + getRight().toStringPostfix() + " " + getOp();
+        } else {
+            return "toStringPostfix() ERROR";
+        }
     }
   
     /*return the expression as a prefix notation string without parenthesis*/
@@ -60,13 +60,13 @@ public class ExpressionTree {
   
     public String toStringPrefix()
     {
-	if (isValue()) {
-	    return getValue() + "";
-	} else if (isOp()) {
-	    return getOp() + " " + getLeft().toStringPrefix() + " " + getRight().toStringPrefix();
-	} else {
-	    return "toStringPrefix() ERROR";
-	}
+        if (isValue()) {
+            return getValue() + "";
+        } else if (isOp()) {
+            return getOp() + " " + getLeft().toStringPrefix() + " " + getRight().toStringPrefix();
+        } else {
+            return "toStringPrefix() ERROR";
+        }
     }  
 
     private char op;
@@ -77,44 +77,44 @@ public class ExpressionTree {
      *  expressions. The can be constructed with a value, or operator and 2
      * sub-ExpressionTrees*/
     public ExpressionTree(double value){
-	this.value = value;
-	op = '~';
+        this.value = value;
+        op = '~';
     }
 
     public ExpressionTree(char op,ExpressionTree l, ExpressionTree r){
-	this.op = op;
-	left = l;
-	right = r;    }  
+        this.op = op;
+        left = l;
+        right = r;    }  
   
     public char getOp(){
-	return op;
+        return op;
     }
   
     /* accessor method for Value, precondition is that isValue() is true.*/
     private double getValue(){
-	return value;
+        return value;
     }
 
     /* accessor method for left, precondition is that isOp() is true.*/
     private ExpressionTree getLeft(){
-	return left;
+        return left;
     }
 
     /* accessor method for right, precondition is that isOp() is true.*/
     private ExpressionTree getRight(){
-	return right;
+        return right;
     }
   
     private boolean isOp(){
-	return hasChildren();
+        return hasChildren();
     }
 
     private boolean isValue(){
-	return !hasChildren();
+        return !hasChildren();
     }
   
     private boolean hasChildren(){
-	return left != null && right != null;
+        return left != null && right != null;
     }
   
 }
